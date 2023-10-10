7714559b7e3e521529df2a64e0081ff624d15990
Zachary Ian May <78890982+ZacharyIanMay@users.noreply.github.com>
2023-10-09 22:10:04 -0700

    formatting

--- PWMan/src/main.rs
+++ PWMan/src/main.rs
@@ -2,13 +2,14 @@ use std::env;
 
 fn main() {
 
-
     println!("OpenSSL works now");
     let dir = env::var("OPENSSL_DIR");
+    
     if dir.is_ok()
     {
         println!("{}", dir.unwrap());
-    } else
+    }
+    else
     {
         println!("error");
     }
