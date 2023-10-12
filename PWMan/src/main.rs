8d973034f3919d5456742bb08763add0ec8b3f65
ZacharyIanMay <zacharymay12383@gmail.com>
2023-10-12 14:09:29 -0700

    Learned to take input and working on fixing string equality not working TODO: fix if statement and string input

--- PWMan/src/main.rs
+++ PWMan/src/main.rs
@@ -1,10 +1,33 @@
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
 use std::fs::OpenOptions;
+use std::io::prelude::*;
+use std::io::stdin;
 
-fn main() -> std::io::Result<()> {
+fn main() -> std::io::Result<()>
+{
+    let mut username = String::new();
+    let mut pass = String::new();
+    let mut s = String::new();
+    let inp = stdin();
+    println!("Please enter your username:");
+    inp.read_line(&mut username)?;
+    if(&username == "test")
+    {
+        println!("Please enter you password:");
+        inp.read_line(&mut pass)?;
+        s = format!("{username}:{pass}");
+    }
+    else
+    {
+        println!("\n\n'{}'", &username);
+        s = "Username isn't 'test'\n".to_string();
+    }
+    
+    add_line(s)
+}
 
+fn add_line(line : String) -> std::io::Result<()> {
     let p = env::var("PROFILE");
     match p
     {
@@ -12,7 +35,8 @@ fn main() -> std::io::Result<()> {
         Ok(pf) =>
         {
             let mut profile = OpenOptions::new().write(true).append(true).create(true).open(&pf)?;
-            profile.write_all(b"\nThis is an appended line")?;
+            let s = format!("{line}");
+            profile.write_all(s.as_bytes())?;
             profile = File::open(&pf)?;
             let mut contents = String::new();
             profile.read_to_string(&mut contents)?;
