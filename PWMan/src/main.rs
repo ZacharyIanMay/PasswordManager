251d4aa595d0c1537520c48fb896047c66b9bc1f
ZacharyIanMay <zacharymay12383@gmail.com>
2023-10-18 14:31:58 -0700

    Managed to make an RSA private and public key from pbkdf2 seed. Untested if it generates the same keys for the same username and password.

--- PWMan/src/main.rs
+++ PWMan/src/main.rs
@@ -3,10 +3,13 @@ use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::prelude::*;
 use std::io::stdin;
+use anyhow::bail;
+use rand::prelude::*;
+use rand_chacha::ChaCha20Rng;
 use base64ct::{Base64, Encoding};
 use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
 use sha2::{Sha512, Digest};
-use anyhow::bail;
+use pbkdf2::{pbkdf2_hmac, pbkdf2_hmac_array};
 
 /**
  * TODO:
@@ -54,15 +57,28 @@ fn main() -> anyhow::Result<()>
     let shash = s.clone();
     let mut hasher = Sha512::new();
     hasher.update(shash);
-    // let salt = generateSalt();
-    // hasher.update(salt);
+    let salt = rand::random::<i32>();
+    hasher.update(salt.to_string());
     let shash = hasher.finalize();
     let shash = Base64::encode_string(&shash);
     println!("{}", shash);
+    println!("{}", salt);
+
+    // key derivation
+    let password = pass.clone();
+    let pbkdf_salt = salt;
+    let n = 210_000;
+    let mut k1 = [0u8; 32];
+    pbkdf2_hmac::<Sha512>(password.as_bytes(), pbkdf_salt.to_string().as_bytes(), n, &mut k1);
+    println!("{:?}", k1);
+
+    let mut seeded_rng = ChaCha20Rng::from_seed(k1);
+    let priv_key = RsaPrivateKey::new(&mut seeded_rng, 2048).expect("failed to generate a key");
+    let pub_key = RsaPublicKey::from(&priv_key);
 
     // Look for a Password based key derivation function, likely PBKDF2 from rustcrypto
-
-    add_line(s)
+    add_line(s)?;
+    add_line(format!("{}\n", salt.to_string()))
 }
 
 fn read_trimmed(s : &mut String) -> anyhow::Result<&str>
