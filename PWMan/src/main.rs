c2352728f49e834708ad7cab5190266d65f6afbb
ZacharyIanMay <zacharymay12383@gmail.com>
2023-10-20 15:22:05 -0700

    figured out how to hable Vec<u8>, converted to hex Lines will be stored as hex formatted RSA encrypted strings TODO: find way to edit specific lines via File.

--- PWMan/src/main.rs
+++ PWMan/src/main.rs
@@ -7,9 +7,10 @@ use anyhow::bail;
 use rand::prelude::*;
 use rand_chacha::ChaCha20Rng;
 use base64ct::{Base64, Encoding};
-use rsa::{Pkcs1v15Encrypt, RsaPrivateKey, RsaPublicKey};
+use rsa::{pkcs8::{DecodePublicKey, EncodePublicKey, DecodePrivateKey, EncodePrivateKey, LineEnding}, RsaPrivateKey, RsaPublicKey, Pkcs1v15Encrypt};
 use sha2::{Sha512, Digest};
 use pbkdf2::{pbkdf2_hmac, pbkdf2_hmac_array};
+use hex::*;
 
 /**
  * TODO:
@@ -39,12 +40,10 @@ fn main() -> anyhow::Result<()>
     let mut username = String::new();
     let mut pass = String::new();
     let mut s = String::new();
-    println!("Please enter your username:");
-    username = read_trimmed(&mut username)?.to_string();
+    username = read_trimmed(&mut username, "Please enter your username:")?;
     if(username == "test")
     {
-        println!("Please enter you password:");
-        read_trimmed(&mut pass)?.to_string();
+        read_trimmed(&mut pass, "Please enter your password:")?;
         s = format!("{username}:{pass}");
     }
     else
@@ -64,29 +63,52 @@ fn main() -> anyhow::Result<()>
     println!("{}", shash);
     println!("{}", salt);
 
-    // key derivation
+    // key derivation, this will be used to verify the user. We thus need to store the salt and the result of the derivation
     let password = pass.clone();
-    let pbkdf_salt = salt;
+    let pbkdf_salt = 1111; // TODO: replace with salt once I've implemented reading salt from file
     let n = 210_000;
     let mut k1 = [0u8; 32];
     pbkdf2_hmac::<Sha512>(password.as_bytes(), pbkdf_salt.to_string().as_bytes(), n, &mut k1);
-    println!("{:?}", k1);
+    //println!("{:?}", k1);
 
+    // This creats an RSA private key
     let mut seeded_rng = ChaCha20Rng::from_seed(k1);
+    let mut srng = seeded_rng.clone();
+    println!("{}", srng.next_u32());
     let priv_key = RsaPrivateKey::new(&mut seeded_rng, 2048).expect("failed to generate a key");
     let pub_key = RsaPublicKey::from(&priv_key);
 
-    // Look for a Password based key derivation function, likely PBKDF2 from rustcrypto
+    let pem = priv_key.to_pkcs8_pem(LineEnding::default())?.to_string();
+    add_line(pem)?;
+
+    // checking encryption and decryption
+    let tbe = "This line is encrypted";
+    println!("{tbe}");
+    let mut rng = rand::thread_rng();
+    let enc = pub_key.encrypt(&mut rng, Pkcs1v15Encrypt, tbe.as_bytes()).expect("failed to encrypt");
+    println!("{:?}", enc);
+    let enc_h = hex::encode(enc);
+    println!("{enc_h}");
+    add_line(enc_h.clone())?;
+    let dec : Vec<u8> = hex::decode(enc_h)?;
+    println!("{:?}", dec);
+    let dec_s = priv_key.decrypt(Pkcs1v15Encrypt, &dec)?;
+    // add_line(std::str::from_utf8(&enc)?.to_string())?;
+    // let dec = priv_key.decrypt(Pkcs1v15Encrypt, &to_dec)?;
+    // add_line(std::str::from_utf8(&dec)?.to_string())?;
+
     add_line(s)?;
-    add_line(format!("{}\n", salt.to_string()))
+    add_line(format!("{}\n", salt.to_string()))?;
+    print_file()
 }
 
-fn read_trimmed(s : &mut String) -> anyhow::Result<&str>
+fn read_trimmed(s : &mut String, query : &str) -> anyhow::Result<String>
 {
+    println!("{}", query);
     let std = stdin();
     std.read_line(s)?;
     let s = s.trim();
-    return Ok(s);
+    return Ok(s.to_string());
 }
 
 fn add_line(line : String) -> anyhow::Result<()> {
@@ -99,7 +121,19 @@ fn add_line(line : String) -> anyhow::Result<()> {
             let mut profile = OpenOptions::new().write(true).append(true).create(true).open(&pf)?;
             let s = format!("{line}");
             profile.write_all(s.as_bytes())?;
-            profile = File::open(&pf)?;
+            return Ok(());
+        }
+    }
+}
+
+fn print_file() -> anyhow::Result<()> {
+    let p = env::var("PROFILE");
+    match p
+    {
+        Err(e) => {bail!("Couldn't find ENV var")}
+        Ok(pf) =>
+        {
+            let mut profile = File::open(&pf)?;
             let mut contents = String::new();
             profile.read_to_string(&mut contents)?;
             println!("File reads:\n{contents}");
